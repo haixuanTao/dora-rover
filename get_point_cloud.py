ef5db5729887a99807f78e6642fc06bdb6d22513
Tao Xavier <tao.xavier@outlook.com>
2022-10-19 10:01:31 -0700

    adding ros bridge

---
+++ get_point_cloud.py
@@ -0,0 +1,17 @@
+import sensor_msgs.point_cloud2 as pc2
+import rospy
+from sensor_msgs.msg import PointCloud2
+import numpy as np
+from dora import Node
+
+node = Node()
+
+def callback(data):
+    gen = pc2.read_points(data, skip_nans=True)
+    data = np.array(list(gen))
+    node.send_output("lidar_pc", data.tobytes())
+    
+rospy.init_node('listener', anonymous=True)
+rospy.Subscriber("os1_points", PointCloud2, callback)
+rospy.spin()
+
